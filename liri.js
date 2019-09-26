d2813c0be3e35cedab6fde1db641b1daf4f5d20b
Jerry Dudum <jsdudum@gmail.com>
2019-09-25 20:08:35 -0700

    created an inquirer choice list which will allow users to pick an option to choose from whether they want to look up bands, songs, movies or do the random txt option

--- liri.js
+++ liri.js
@@ -7,4 +7,33 @@ var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
 var fs = require("fs");
 
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Please select a command to run.",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+            name: "options"
+        }
+    ])
+    .then(function(result){
 
+        if (result.options === "concert-this") {
+            concertThis();
+        }
+
+        if (result.options === "spotify-this-song") {
+            spotifyThisSong();
+        }
+
+        if (result.options === "movie-this") {
+            movieThis();
+        }
+
+        if (result.options === "do-what-it-says") {
+            doThis();
+        }
+
+    });
+
+    
