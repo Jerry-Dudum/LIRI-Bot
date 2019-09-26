0046144601b3ef8617eac8f9076f462017f6e1c0
Jerry Dudum <jsdudum@gmail.com>
2019-09-25 21:52:28 -0700

    added last do what it says function and it is dynamic where if you do switch text in the random txt file as long as it is one of the initial choices it will run that function for the next parameter given after a comma

--- liri.js
+++ liri.js
@@ -162,4 +162,88 @@ function movieThis() {
                     console.log(error.config);
                 });
         })
+};
+
+var option = "";
+
+function doThis() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+
+        option = dataArr[0];
+        random = dataArr[1];
+
+        switch (option) {
+            case "concert-this":
+                axios.get("https://rest.bandsintown.com/artists/" + random + "/events?app_id=codingbootcamp").then(function (response) {
+                    
+
+                    if (response.data.length === 0) {
+                        console.log("Sorry no upcoming events for this artist :(")
+                    }
+                    else {
+                        console.log("Venue: " + response.data[0].venue.name);
+                        console.log("Venue Location: " + response.data[0].venue.city + ", " + response.data[0].venue.region);
+
+                        date = moment(response.data[0].datetime).format('MM DD YYYY')
+                        console.log("Date: " + date);
+                    }
+
+
+                })
+                break;
+
+            case "spotify-this-song":
+                spotify.search({ type: 'track', query: random }, function (err, data) {
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    }
+
+                    console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+                    console.log("Song Name: " + data.tracks.items[0].name);
+                    console.log("Album: " + data.tracks.items[0].album.name);
+                    if (data.tracks.items[0].preview_url) {
+                        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+                    }
+
+                })
+                break;
+            
+            case "movie-this":
+                axios.get("http://www.omdbapi.com/?t=" + random + "&y=&plot=short&apikey=trilogy").then(
+                    function (response) {
+                        
+
+                        console.log("Title: " + response.data.Title);
+                        console.log("Released: " + response.data.Released);
+                        console.log("IMDB Rating: " + response.data.imdbRating);
+                        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                        console.log("Country Produced: " + response.data.Country);
+                        console.log("Language of Movie: " + response.data.Language);
+                        console.log("Plot: " + response.data.Plot);
+                        console.log("Actors: " + response.data.Actors);
+                    })
+                    .catch(function (error) {
+                        if (error.response) {
+                            console.log("---------------Data---------------");
+                            console.log(error.response.data);
+                            console.log("---------------Status---------------");
+                            console.log(error.response.status);
+                            console.log("---------------Status---------------");
+                            console.log(error.response.headers);
+                        } else if (error.request) {
+                            console.log(error.request);
+                        } else {
+                            console.log("Error", error.message);
+                        }
+                        console.log(error.config);
+                    });
+                break;
+        }
+    });
 }
