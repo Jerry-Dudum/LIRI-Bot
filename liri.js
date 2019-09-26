46dd269f71acabcb33e92185050f3872a15634a4
Jerry Dudum <jsdudum@gmail.com>
2019-09-25 21:34:44 -0700

    added functionality to search movies option

--- liri.js
+++ liri.js
@@ -113,3 +113,53 @@ function spotifyThisSong() {
             })
         })
 };
+
+var movie = "";
+
+function movieThis() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please select a movie to search up.",
+                name: "movie"
+            }
+        ])
+        .then(function (result) {
+            if (result.movie === "") {
+                console.log("You didn't type out a movie so I'll choose Eternal Sunshine of the Spotless Mind for you.");
+                movie = "Eternal Sunshine of the Spotless Mind";
+            }
+            else {
+                movie = result.movie;
+            }
+            axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy").then(
+                function (response) {
+                    // console.log(response.data); used to look up values and parse data
+
+                    console.log("Title: " + response.data.Title);
+                    console.log("Released: " + response.data.Released);
+                    console.log("IMDB Rating: " + response.data.imdbRating);
+                    console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                    console.log("Country Produced: " + response.data.Country);
+                    console.log("Language of Movie: " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot);
+                    console.log("Actors: " + response.data.Actors);
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        console.log("---------------Data---------------");
+                        console.log(error.response.data);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.status);
+                        console.log("---------------Status---------------");
+                        console.log(error.response.headers);
+                    } else if (error.request) {
+                        console.log(error.request);
+                    } else {
+                        console.log("Error", error.message);
+                    }
+                    console.log(error.config);
+                });
+        })
+}
